b66e9082a486b7b1cc4238e99ce761f4daa3bb90
yuchi0307 <gelee0307@gmail.com>
2021-08-21 22:21:24 +0800

    多個 useState 和只用1個 useState 的寫法

--- src/Components/NewExpense/ExpenseForm.js
+++ src/Components/NewExpense/ExpenseForm.js
@@ -1,20 +1,56 @@
 import "./ExpenseForm.css";
+import { useState } from "react";
 
 const ExpenseForm = () => {
+
+  // const [enteredTitle, setEnteredTitle] = useState(''); //一開始欄位都是空白，所以給空字串
+  // const [enteredAmount, setEnteredAmount] = useState(''); //即便輸入的看似是數字，但型態依舊是字串
+  // const [enteredDate, setEnteredDate] = useState('');
+  const [userInput, setUserInput] = useState({
+    enteredTitle: '',
+    enteredAmount: '',
+    enteredDate: ''
+  });
+
+  const titleChangeHandler = (event) =>{
+    //setEnteredTitle(event.target.value);
+    setUserInput({
+      ...userInput,
+      enteredTitle: event.target.value
+    });
+  };
+
+  const amountChangeHandler = (event) => {
+    //setEnteredAmount(event.target.value);
+    setUserInput({
+      ...userInput,
+      enteredAmount: event.target.value
+    });
+  };
+
+  const dateChangeHandler = (event) => {
+    //setEnteredDate(event.target.value);
+    setUserInput({
+      ...userInput,
+      enteredDate: event.target.value
+    })
+
+  }
+
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" onChange={titleChangeHandler}/>
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0" step="1" />
+          <input type="number" min="0" step="1" onChange={amountChangeHandler}/>
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2020-03-07" max="2025-03-07" />
+          <input type="date" min="2020-03-07" max="2025-03-07" onChanege={dateChangeHandler}/>
         </div>
         <div className="new_expense__actions">
           <button type="submit">Add expense</button>
