e252298428e9a50abc60124dc986f599e920bb22
yuchi0307 <gelee0307@gmail.com>
2021-08-23 22:26:42 +0800

    提交表單並清空欄位

--- src/Components/NewExpense/ExpenseForm.js
+++ src/Components/NewExpense/ExpenseForm.js
@@ -1,62 +1,93 @@
-import "./ExpenseForm.css";
-import { useState } from "react";
+import React, { useState } from 'react';
+
+import './ExpenseForm.css';
 
 const ExpenseForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState(''); //一開始欄位都是空白，所以給空字串
+  const [enteredAmount, setEnteredAmount] = useState(''); //即便輸入的看似是數字，但型態依舊是字串
+  const [enteredDate, setEnteredDate] = useState('');
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle: '',
+  //   enteredAmount: '',
+  //   enteredDate: '',
+  // });
 
-  // const [enteredTitle, setEnteredTitle] = useState(''); //一開始欄位都是空白，所以給空字串
-  // const [enteredAmount, setEnteredAmount] = useState(''); //即便輸入的看似是數字，但型態依舊是字串
-  // const [enteredDate, setEnteredDate] = useState('');
-  const [userInput, setUserInput] = useState({
-    enteredTitle: '',
-    enteredAmount: '',
-    enteredDate: ''
-  });
-
-  const titleChangeHandler = (event) =>{
-    //setEnteredTitle(event.target.value);
-    setUserInput({
-      ...userInput,
-      enteredTitle: event.target.value
-      //在 react 中，這個 state 被更新了，若沒有顧及其他兩個物件，新的 state 將不會保留就有的其他物件
-      //所以將更新 state 時，用...userInput先保留所有物件，再 override title，以此類推
-    });
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredTitle: event.target.value,
+    // });
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredTitle: event.target.value };
+    // 如果要用到先前的 state 值，最好使用這個寫法 
+    // });
   };
 
   const amountChangeHandler = (event) => {
-    //setEnteredAmount(event.target.value);
-    setUserInput({
-      ...userInput,
-      enteredAmount: event.target.value
-    });
+    setEnteredAmount(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredAmount: event.target.value,
+    // });
   };
 
   const dateChangeHandler = (event) => {
-    //setEnteredDate(event.target.value);
-    setUserInput({
-      ...userInput,
-      enteredDate: event.target.value
-    })
+    setEnteredDate(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredDate: event.target.value,
+    // });
+  };
 
-  }
+  const submitHandler = (event) => {
+    event.preventDefault(); ////和發送 request 有關的js
+    
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate) ////輸入的日期是字串，把它轉成date物件
+    };
+
+    console.log(expenseData);
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
+    //按下送出表單後清空欄位
+  };
 
   return (
-    <form>
-      <div className="new-expense__controls">
-        <div className="new-expense__control">
+    <form onSubmit={submitHandler}>
+      <div className='new-expense__controls'>
+        <div className='new-expense__control'>
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler}/>
+          <input type='text' 
+          value={enteredTitle} 
+          onChange={titleChangeHandler} />
         </div>
-        <div className="new-expense__control">
+        <div className='new-expense__control'>
           <label>Amount</label>
-          <input type="number" min="0" step="1" onChange={amountChangeHandler}/>
+          <input
+            type='number'
+            min='1'
+            step='1'
+            value={enteredAmount} //不只可以監聽輸入欄位，還可以在 sumbit 後將值清空
+            onChange={amountChangeHandler}
+          />
         </div>
-        <div className="new-expense__control">
+        <div className='new-expense__control'>
           <label>Date</label>
-          <input type="date" min="2020-03-07" max="2025-03-07" onChanege={dateChangeHandler}/>
+          <input
+            type='date'
+            min='2019-01-01'
+            max='2022-12-31'
+            value={enteredDate}
+            onChange={dateChangeHandler}
+          />
         </div>
-        <div className="new_expense__actions">
-          <button type="submit">Add expense</button>
-        </div>
+      </div>
+      <div className='new-expense__actions'>
+        <button type='submit'>Add Expense</button>
       </div>
     </form>
   );
